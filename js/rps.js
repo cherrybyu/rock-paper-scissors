f42a22883f57f67f73bdabea8b7b4397b7b63657
hanabyu <cherrybyuu@gmail.com>
2022-10-05 20:27:17 -0700

    Update rps.js

--- js/rps.js
+++ js/rps.js
@@ -1,4 +1,4 @@
-const choiceList = ['rock', 'paper', 'scissors'];
+const choiceList = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * choiceList.length);
@@ -18,14 +18,35 @@ function getPLayerChoice() {
     return playerChoice;
 }
 
-const playerSelection = getPLayerChoice90;
+const playerSelection = getPLayerChoice();
 const computerSelection = getComputerChoice();
 
 
 function playRound(playerSelection, computerSelection){
-    
-}
-    
+    if (playerSelection == computerSelection) {
+        alert("It's a draw!");
+        return "It's a draw!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        alert("You lose! Better luck next time!");
+        return "You lose! Better luck next time!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        alert("You won! Congratultions!");
+        return "You won! Congratultions!";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        alert("You lose! Better luck next time!");
+        return "You lose! Better luck next time!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        alert("You won! Congratulations!");
+        return "You won! Congratulations!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        alert("You lose! Better luck next time!");
+        return "You lose! Better luck next time!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        alert("You won! Congratulations!");
+        return "You won! Congratulations!";
+    }
+} 
+
 
 
 
@@ -36,5 +57,6 @@ function getComputerChoice will randomly return rock, paper, or scissors
 
 
 */
-console.log(getPLayerChoice())
-console.log(getComputerChoice())
+
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection))
